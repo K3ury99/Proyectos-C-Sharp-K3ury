c3a1fddb2b8298a80c652f2eedc9615e664cef38
K3ury <keury2004@hotmail.com>
2024-02-19 23:07:11 -0400

    Proyecto: Adivinanzas mejorado

--- Adivinanzas/obj/Debug/net7.0/Adivinanzas.AssemblyInfo.cs
+++ Adivinanzas/obj/Debug/net7.0/Adivinanzas.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Adivinanzas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1366fb785a9aa9c4bc2b875287c560e728423ea2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Adivinanzas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Adivinanzas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
